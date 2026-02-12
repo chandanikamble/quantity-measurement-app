57ce2891fe4a07d68408fa1d99797d0f082b7a82
chandanikamble <kamblechandani369@gmail.com>
2026-02-12 11:53:28 +0530

    updated code

--- src/main/java/org/example/QuantityMeasurementApp.java
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -2,56 +2,51 @@ package org.example;
 
 public class QuantityMeasurementApp{
 
-    public static void demonstrateFeetEquality(){
-        Length f1 = new Length(1.0, Length.LengthUnit.FEET);
-        Length f2 = new Length(1.0, Length.LengthUnit.FEET);
-        boolean result = f1.equals(f2);
-        System.out.println("Feet Equality Result :" + result);
+    public static void demonstrateYardFeetEquality(){
+        Length l1 = new Length(1.0, Length.LengthUnit.YARDS);
+        Length l2 = new Length(3.0, Length.LengthUnit.FEET);
+        boolean result = l1.equals(l2);
+        System.out.println( 1.0 + " " + Length.LengthUnit.YARDS + " == " +
+                3.0 + " " + Length.LengthUnit.FEET + " : " + result );
     }
 
-    public static void demonstrateInchesEquality(){
-        Length i1 = new Length(9.0, Length.LengthUnit.INCHES);
-        Length i2 = new Length(8.0, Length.LengthUnit.INCHES);
-        boolean result = i1.equals(i2);
-        System.out.println("Inches Equality Result:" + result);
+    public static void demonstrateYardInchesEquality(){
+        Length l1 = new Length(1.0, Length.LengthUnit.YARDS);
+        Length l2 = new Length(36.0, Length.LengthUnit.INCHES);
+        boolean result = l1.equals(l2);
+        System.out.println( 1.0 + " " + Length.LengthUnit.YARDS + " == " +
+                36.0 + " " + Length.LengthUnit.INCHES + " : " + result );
     }
 
-    public static void demonstrateFeetInchesComparison(){
-        Length feet = new Length(3.0, Length.LengthUnit.FEET);
-        Length inches = new Length(24.0, Length.LengthUnit.INCHES);
-        boolean result = feet.equals(inches);
-        System.out.println("Are Lengths equals :" + result);
+    public static void demonstrateYardEquality(){
+        Length l1 = new Length(2.0, Length.LengthUnit.YARDS);
+        Length l2 = new Length( 2.0, Length.LengthUnit.YARDS);
+        boolean result = l1.equals(l2);
+        System.out.println( 2.0 + " " + Length.LengthUnit.YARDS + " == " +
+                2.0 + " " + Length.LengthUnit.YARDS + " : " + result );
     }
 
-    public static void main(String[] args){
-        demonstrateFeetEquality();
-        demonstrateInchesEquality();
-        demonstrateFeetInchesComparison();
+    public static void demonstrateCentimetersEquality(){
+        Length l1 = new Length(2.0, Length.LengthUnit.CENTIMETERS);
+        Length l2 = new Length(2.0, Length.LengthUnit.CENTIMETERS);
+        boolean result = l1.equals(l2);
+        System.out.println( 2.0 + " " + Length.LengthUnit.CENTIMETERS + " == " +
+                2.0 + " " + Length.LengthUnit.CENTIMETERS + " : " + result );
+    }
+
+    public static void demonstrateCMSInchesEquality(){
+        Length l1 = new Length(1.0, Length.LengthUnit.CENTIMETERS);
+        Length l2 = new Length(0.393701, Length.LengthUnit.INCHES);
+        boolean result = l1.equals(l2);
+        System.out.println( 1.0 + " " + Length.LengthUnit.CENTIMETERS + " == " +
+                0.393701 + " " + Length.LengthUnit.INCHES + " : " + result );
+    }
+
+    public static void main(String[] args) {
+        demonstrateYardFeetEquality();
+        demonstrateYardInchesEquality();
+        demonstrateYardEquality();
+        demonstrateCentimetersEquality();
+        demonstrateCMSInchesEquality();
     }
 }
-
-
-
-//public class QuantityMeasurementApp {
-//
-//    public static void demonstrateEquality(
-//            double value1, Length.LengthUnit unit1,
-//            double value2, Length.LengthUnit unit2) {
-//
-//        Length l1 = new Length(value1, unit1);
-//        Length l2 = new Length(value2, unit2);
-//
-//        System.out.println(
-//                value1 + " " + unit1 + " == " +
-//                        value2 + " " + unit2 + " : " +
-//                        l1.equals(l2)
-//        );
-//    }
-//
-//    public static void main(String[] args) {
-//        demonstrateEquality(1.0, Length.LengthUnit.FEET, 12.0, Length.LengthUnit.INCHES);
-//        demonstrateEquality(1.0, Length.LengthUnit.YARDS, 36.0, Length.LengthUnit.INCHES);
-//        demonstrateEquality(30.48, Length.LengthUnit.CENTIMETERS, 1.0, Length.LengthUnit.FEET);
-//        demonstrateEquality(3.0, Length.LengthUnit.FEET, 1.0, Length.LengthUnit.YARDS);
-//    }
-//}

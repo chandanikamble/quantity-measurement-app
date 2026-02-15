4a0da71dbfc5defc98a573fcc890200e122375c9
chandanikamble <kamblechandani369@gmail.com>
2026-02-15 22:02:51 +0530

    use case

--- src/main/java/org/example/Length.java
+++ src/main/java/org/example/Length.java
@@ -22,14 +22,47 @@ public class Length {
     }
 
     public Length(double value, LengthUnit unit){
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Value must be a finite number.");
+        }
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null.");
+        }
+
         this.value = value;
         this.unit = unit;
     }
 
-    private double convertToBaseUnit(){
-        return value * unit.getConversionFactor();
+    public double convertTo(LengthUnit targetUnit) {
+        return convert(this.value, this.unit, targetUnit);
     }
 
+    //    // Static conversion method (as required)
+    public static double convert(double value,
+                                 LengthUnit sourceUnit,
+                                 LengthUnit targetUnit) {
+
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Value must be finite.");
+        }
+        if (sourceUnit == null || targetUnit == null) {
+            throw new IllegalArgumentException("Units cannot be null.");
+        }
+
+        // Convert to base unit (INCHES)
+        double valueInBase = value * sourceUnit.getConversionFactor();
+
+        // Convert to target
+        double result = valueInBase / targetUnit.getConversionFactor();
+
+        // Optional rounding (4 decimal places)
+        return Math.round(result * 10000.0) / 10000.0;
+    }
+
+    // Convert current value to base (INCHES)
+    private double toBase() {
+        return value * unit.getConversionFactor();
+    }
 
     @Override()
     public boolean equals(Object obj){
@@ -43,6 +76,23 @@ public class Length {
 
         Length other = (Length) obj;
 
-        return Double.compare(this.convertToBaseUnit(), other.convertToBaseUnit()) == 0;
+        return Double.compare(this.toBase(), other.toBase()) == 0;
     }
+
+
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) return true;
+//        if (!(obj instanceof Length)) return false;
+//
+//        Length other = (Length) obj;
+//
+//        return Double.compare(this.toBase(), other.toBase()) == 0;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Double.hashCode(toBase());
+//    }
 }
--- src/main/java/org/example/QuantityMeasurementApp.java
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -1,57 +1,37 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class QuantityMeasurementApp{
 
-    public static void demonstrateFeetEquality(){
-        Length f1 = new Length(1.0, Length.LengthUnit.FEET);
-        Length f2 = new Length(1.0, Length.LengthUnit.FEET);
-        boolean result = f1.equals(f2);
-        System.out.println("Feet Equality Result :" + result);
-    }
-
-    public static void demonstrateInchesEquality(){
-        Length i1 = new Length(9.0, Length.LengthUnit.INCHES);
-        Length i2 = new Length(8.0, Length.LengthUnit.INCHES);
-        boolean result = i1.equals(i2);
-        System.out.println("Inches Equality Result:" + result);
-    }
-
     public static void demonstrateFeetInchesComparison(){
-        Length feet = new Length(3.0, Length.LengthUnit.FEET);
-        Length inches = new Length(24.0, Length.LengthUnit.INCHES);
-        boolean result = feet.equals(inches);
-        System.out.println("Are Lengths equals :" + result);
+//        Length feet = new Length(3.0, Length.LengthUnit.FEET);
+//        Length inches = new Length(24.0, Length.LengthUnit.INCHES);
+//        boolean result = feet.equals(inches);
+//        System.out.println("Are Lengths equals :" + result);
     }
 
     public static void main(String[] args){
-        demonstrateFeetEquality();
-        demonstrateInchesEquality();
-        demonstrateFeetInchesComparison();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value : ");
+        double value = sc.nextDouble();
+
+        System.out.print("Enter Unit : ");
+        String unit1 = sc.next().toUpperCase();
+
+        System.out.print("Enter Target Unit : ");
+        String unit2 = sc.next().toUpperCase();
+
+        Length.LengthUnit sourceUnit = Length.LengthUnit.valueOf(unit1);
+        Length.LengthUnit targetUnit = Length.LengthUnit.valueOf(unit2);
+
+        Length length = new Length(value, sourceUnit);
+
+        double result = length.convertTo(targetUnit);
+//        double result = Length.convert(value, sourceUnit, targetUnit);
+
+        System.out.println("Converted value: " + result + " " + targetUnit);
     }
 }
 
-
-
-//public class QuantityMeasurementApp {
-//
-//    public static void demonstrateEquality(
-//            double value1, Length.LengthUnit unit1,
-//            double value2, Length.LengthUnit unit2) {
-//
-//        Length l1 = new Length(value1, unit1);
-//        Length l2 = new Length(value2, unit2);
-//
-//        System.out.println(
-//                value1 + " " + unit1 + " == " +
-//                        value2 + " " + unit2 + " : " +
-//                        l1.equals(l2)
-//        );
-//    }
-//
-//    public static void main(String[] args) {
-//        demonstrateEquality(1.0, Length.LengthUnit.FEET, 12.0, Length.LengthUnit.INCHES);
-//        demonstrateEquality(1.0, Length.LengthUnit.YARDS, 36.0, Length.LengthUnit.INCHES);
-//        demonstrateEquality(30.48, Length.LengthUnit.CENTIMETERS, 1.0, Length.LengthUnit.FEET);
-//        demonstrateEquality(3.0, Length.LengthUnit.FEET, 1.0, Length.LengthUnit.YARDS);
-//    }
-//}

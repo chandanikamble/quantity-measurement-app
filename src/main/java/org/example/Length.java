e61ceeebd3e0b154013440e0e96c686329f6fe7f
chandanikamble <kamblechandani369@gmail.com>
2026-02-24 15:57:12 +0530

    code update

--- src/main/java/org/example/Length.java
+++ src/main/java/org/example/Length.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Length {
     private double value;
     private LengthUnit unit;
@@ -25,37 +27,23 @@ public class Length {
         if (!Double.isFinite(value)) {
             throw new IllegalArgumentException("Value must be a finite number.");
         }
-        if (unit == null) {
-            throw new IllegalArgumentException("Unit cannot be null.");
-        }
 
         this.value = value;
         this.unit = unit;
     }
 
-    public double convertTo(LengthUnit targetUnit) {
-        return convert(this.value, this.unit, targetUnit);
+    public static Length add(Length l1, Length l2){
+        if (l1 == null || l2 == null) {
+            throw new NullPointerException("Length must not be null.");
+        }
+        double sum = l1.toBase() + l2.toBase();
+        double convertedValue = convert(sum, l1.unit);
+        return new Length(convertedValue, l1.unit);
     }
 
-    //    // Static conversion method (as required)
-    public static double convert(double value,
-                                 LengthUnit sourceUnit,
-                                 LengthUnit targetUnit) {
-
-        if (!Double.isFinite(value)) {
-            throw new IllegalArgumentException("Value must be finite.");
-        }
-        if (sourceUnit == null || targetUnit == null) {
-            throw new IllegalArgumentException("Units cannot be null.");
-        }
-
-        // Convert to base unit (INCHES)
-        double valueInBase = value * sourceUnit.getConversionFactor();
-
+    public static double convert(double valueInBase, LengthUnit targetUnit) {
         // Convert to target
         double result = valueInBase / targetUnit.getConversionFactor();
-
-        // Optional rounding (4 decimal places)
         return Math.round(result * 10000.0) / 10000.0;
     }
 
@@ -71,33 +59,18 @@ public class Length {
 
     @Override()
     public boolean equals(Object obj){
-        if(this == obj){
-            return true;
-        }
-
+        if(this == obj){ return true; }
         if(obj == null || this.getClass() != obj.getClass()){
             return false;
         }
 
         Length other = (Length) obj;
-
         return Double.compare(this.toBase(), other.toBase()) == 0;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash();
 
-//
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (this == obj) return true;
-//        if (!(obj instanceof Length)) return false;
-//
-//        Length other = (Length) obj;
-//
-//        return Double.compare(this.toBase(), other.toBase()) == 0;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Double.hashCode(toBase());
-//    }
+    }
 }
